8320cc3aa18a6bc77174647c421334660a02a2d5
zeenat <zeenu.23@gmail.com>
2024-07-12 20:01:06 +0530

    update App.css with new modal-content styles

--- src/Modal.jsx
+++ src/Modal.jsx
@@ -10,7 +10,7 @@ function Modal({ isOpen, onClose }) {
         <button onClick={onClose}>&times;</button>      
       </div>
       <div className="modal-content">
-        <p>This is a modal content with lots of text</p>    
+        <p>This is a modal content with lots of text. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, molestias alias. Dolore quam nam voluptatibus repellat corrupti, qui dolor maxime ipsa sapiente sunt, ab aliquam culpa obcaecati eius cum delectus? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit, rem.</p>    
       </div>
       <div className="modal-footer">
         <p>Modal Footer</p> 
