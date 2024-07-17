0fa0e8e01851510b52f6d4ff699ff35be7498e66
zeenat <zeenu.23@gmail.com>
2024-07-17 13:59:03 +0530

    refactor Users component

--- src/Users.jsx
+++ src/Users.jsx
@@ -1,23 +1,43 @@
 import { useState, useEffect } from 'react'
 
-function Users() {
-    const [users, setUsers] = useState([]) 
-    const [isLoading, setIsLoading] = useState(true)
-
-    const getUsersData = async () => {
+ const getUsersData = async () => {
         const res = await fetch('https://jsonplaceholder.typicode.com/users')
         const data = await res.json()
         console.log(data)
+}
+
+function Users() {
+    const [users, setUsers] = useState([]) 
+    const [isLoading, setIsLoading] = useState(false)
+    const [searchText, setSearchText] = useState("")
+
+    useEffect(() => {
+       setIsLoading(true)
+        getUsersData()
+            .then((res) => setUsers(res))
+        .finally(setIsLoading(false))
+    }, [])
+    
+    function handleSearch(e) {
+       setSearchText(e.target.value) 
     }
 
-   useEffect(() => {
-    getUsersData()
-   }, [])
-
   return (
-    <div>
-      
-    </div>
+    <>
+      <input value={users} onChange={handleSearch} placeholder='Search...' />
+      <thead>
+        <tr>
+            <th>Name</th>
+            <th>Username</th>
+            <th>Email</th>
+            <th>Phone</th>
+            <th>Company Name</th>
+        </tr>
+       </thead>
+        <tbody>
+              
+       </tbody>
+    </>
   )
 }
 
